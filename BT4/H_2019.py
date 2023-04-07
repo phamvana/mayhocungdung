c30846e2a012e18b93dfb32021f5997386507536
Pham Van A <phamvana@gmail.com>
2023-04-07 18:36:07 +0700

    Bai tap 4

--- BT4/H_2019.py
+++ BT4/H_2019.py
@@ -11,10 +11,10 @@ dt.iloc[2:4,]
 #Giá trị X
 X=dt.iloc[:,[1,2,3,4,10]]
 X.iloc[1:5,]
-#print(X)
+print(X)
 
 Y = dt.price
-#print(Y)
+print(Y)
 
 '''
 Hiển thị dữ liệu tương quan giữa diện tích (lotsize) và giá nhà (price)
@@ -25,4 +25,25 @@ plt.show()
 
 # Huan luyen mo hinh
 
-nl = linear_model.LinearRegression()
+lm = linear_model.LinearRegression()
+lm.fit(X[1:520],Y[1:520])
+
+print(round(lm.intercept_,3))
+print(lm.coef_)
+
+# du bao gi nha cho 20 phan tu cuoi cung trong tap du lieu
+y = dt.price
+y_test =Y[-20:]
+x_test=X[-20:]
+y_pred = lm.predict(x_test)
+#so sanh gia tri du bao va gia tri thuc te
+print("Y_pred",y_pred)
+print("y_test: ",y_test)
+
+# danh gia
+from sklearn.metrics import mean_squared_error
+err = mean_squared_error(y_test,y_pred)
+print(round(err,3))
+np.sqrt(err)
+
+print("SQRT ERR",np.sqrt(err))
